1607d86045ef17ac2cd65b71afb6adc3db560dbb
Tsukamoto <tanabon2113@gmail.com>
2026-06-28 14:44:27 +0900

    WIP:BookController実装途中

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+
+// 仮のルーティング
+Route::get('/ranking', [RankingController::class,])->name('ranking.index');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+    Route::post('/books/create', [BookController::class, 'store'])->name('books.store');
+    Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+    Route::put('/books/{book}/edit', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+    // 仮のルーティング
+    Route::get('/favorites', [FavoriteController::class,])->name('favorites.index');
+    Route::get('/genres', [GenreController::class,])->name('genres.index');
 });
