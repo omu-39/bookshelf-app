0481577e37b3fad0288205b07ad0a792e1d510db
Tsukamoto <tanabon2113@gmail.com>
2026-06-29 17:52:39 +0900

    fix/バリデーションメッセージの修正

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -32,6 +32,7 @@ class CreateNewUser implements CreatesNewUsers
                 Rule::unique(User::class),
             ],
             'password' => $this->passwordRules(),
+            'password_confirmation' => 'required',
         ])->validate();
 
         return User::create([
