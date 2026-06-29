c68d8cb49c44b9d0b38fd772ce26e8813f2d2a67
Tsukamoto <tanabon2113@gmail.com>
2026-06-29 18:03:19 +0900

    feat:ReviewRequestの作成

---
+++ app/Http/Requests/ReviewRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'rating' => ['required', 'integer', 'in:1,2,3,4,5'],
+            'comment' => ['required', 'string', 'max:1000'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'rating.required' => '評価は​必須です。',
+            'rating.integer' => '評価は​整数で​入力してください。',
+            'rating.in' => '評価は1〜5の整数で入力してください。',
+            'comment.required' => 'コメントは​必須です。​',
+            'comment.string' => 'コメントは​文字列で​入力してください。​',
+            'comment.max' => 'コメントは1000文字以下で入力してください。',
+        ];
+    }
+}
