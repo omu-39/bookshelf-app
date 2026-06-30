5960a397636c3240ef9d11ecd02fb1b7d0331f5d
Tsukamoto <tanabon2113@gmail.com>
2026-06-30 19:19:59 +0900

    feat:IndexBookRequestの作成

---
+++ app/Http/Requests/Api/V1/IndexBookRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'keyword' => ['nullable', 'string', 'max:255'],
+            'genres' => ['nullable', 'array'],
+            'genres.*' => ['integer', 'exists:genres,id'],
+            'page' => ['nullable', 'integer', 'min:1'],
+            'per_page' => ['nullable', 'integer', 'between:1,100'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'keyword.string' => '検索キーワードは​文字列で​入力してください。',
+            'keyword.max' => '検索キーワードは255文字以内で入力してください。',
+            'genres.array' => 'ジャンルは配列で入力してください',
+            'genres.*.exists' => '選択されたジャンルは存在しません。',
+            'page.integer' => 'ページ​番号が​正しく​ありません。',
+            'page.min' => 'ページ番号は1以上の数値を指定してください。',
+            'per_page.integer' => '表示件数の​指定が​正しく​ありません。',
+            'per_page.between' => '表示件数は1〜100件の間で指定してください。'
+        ];
+    }
+}
