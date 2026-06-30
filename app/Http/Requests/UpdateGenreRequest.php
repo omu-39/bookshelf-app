05c76e30123c3c658284ca144b5bce52d9952942
Tsukamoto <tanabon2113@gmail.com>
2026-06-30 13:00:20 +0900

    feat:StoreGenreRequest, UpdateGenreRequestの作成

---
+++ app/Http/Requests/UpdateGenreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateGenreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', Rule::unique('genres', 'name')->ignore($this->genre)]
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'ジャンル名は​必須です。',
+            'name.string' => 'ジャンル名は​文字列で​入力してください。​',
+            'name.unique' => 'その​ジャンル名は​既に​使用されています。​',
+        ];
+    }
+}
