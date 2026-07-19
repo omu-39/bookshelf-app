07610ddb21ae2a6bd89cc84894fd466954a5c4aa
Tsukamoto <tanabon2113@gmail.com>
2026-07-19 17:05:17 +0900

    fix:add-unique-rule

--- app/Http/Requests/StoreReadingPlanRequest.php
+++ app/Http/Requests/StoreReadingPlanRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ReadingPlanRequest extends FormRequest
+class StoreReadingPlanRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,7 +22,7 @@ class ReadingPlanRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'book_id' => ['required', 'exists:books,id'],
+            'book_id' => ['required', 'exists:books,id', 'unique:reading_plans,book_id'],
             'target_date' => ['required', 'date', 'after:today'],
         ];
     }
@@ -32,6 +32,7 @@ class ReadingPlanRequest extends FormRequest
         return [
             'book_id.required' => '書籍は必須です。',
             'book_id.exists' => '登録済みの書籍から選択してください。',
+            'book_id.unique' => 'その書籍の読書計画は登録済みです。',
             'target_date.required' => '期日は必須です。',
             'target_date.date' => '期日は​有効な​日付形式で​入力してください。',
             'target_date.after' => '期日は翌日以降を入力してください。'
