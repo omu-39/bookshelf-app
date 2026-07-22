8bb350c9879b3eee95cfd2bd5aaee499c3e8c7e6
Tsukamoto <tanabon2113@gmail.com>
2026-07-22 14:35:33 +0900

    feat:update-BookModelTest

--- tests/Unit/BookModelTest.php
+++ tests/Unit/BookModelTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Models\Book;
 use App\Models\Genre;
+use App\Models\ReadingPlan;
 use App\Models\Review;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -22,10 +23,12 @@ class BookModelTest extends TestCase
         $review = Review::factory()->for($anotherUser)->for($book)->create();
         $book->favoritedUsers()->attach($anotherUser);
         $book->genres()->attach($genre);
+        $readingPlan = ReadingPlan::factory()->for($book)->create();
 
         $this->assertTrue($book->user->is($user));
         $this->assertTrue($book->genres->contains($genre));
         $this->assertTrue($book->reviews->contains($review));
         $this->assertTrue($book->favoritedUsers->contains($anotherUser));
+        $this->assertTrue($book->readingPlans->contains($readingPlan));
     }
 }
