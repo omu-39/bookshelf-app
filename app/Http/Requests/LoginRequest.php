0918f8b7f6bd290d6756ef60be058674b6cb15b6
Tsukamoto <tanabon2113@gmail.com>
2026-07-31 20:50:39 +0900

    feat:add LoginRequest

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Laravel\Fortify\Http\Requests\LoginRequest as FortifyLoginRequest;
+
+//デフォルトのログイン機能には上述のFortifyLoginRequestクラスが引数として渡されているため、異なるクラスを呼び出すとエラーが出力される。
+//そのため、デフォルトにある上述のFortifyLoginRequestクラスを継承する必要がある
+
+class LoginRequest extends FortifyLoginRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'email' => ['required'],
+            'password' => ['required']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'メールアドレスを入力してください',
+            'password.required' => 'パスワードを入力してください',
+        ];
+    }
+}
